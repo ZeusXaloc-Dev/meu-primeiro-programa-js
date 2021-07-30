3c77de6659afab82701a9467db3011e797724b04
Zeus Xaloc Dev <85583693+ZeusXaloc-Dev@users.noreply.github.com>
2021-07-29 21:34:10 -0400

    Add files via upload

---
+++ one-script.js
@@ -0,0 +1,11 @@
+function sayHello() {
+    // meu primeiro programa
+    let name = prompt("Enter your name:");
+
+    // Meu Hello
+    console.log("Hello, " + name + "!");
+}
+
+
+// Resultado
+sayHello();
